54987e6a0168ca4791f6ffc561b7051d726649f9
Alek Novkovski <alek@greatcode2.com>
2023-02-14 16:43:07 +0100

    Replace mode switching btn w Link to change mode

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,4 +1,4 @@
-import {Form, useSearchParams} from 'react-router-dom';
+import {Form, Link, useSearchParams} from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
@@ -19,9 +19,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button type="button">
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </Link>
           <button>Save</button>
         </div>
       </Form>
