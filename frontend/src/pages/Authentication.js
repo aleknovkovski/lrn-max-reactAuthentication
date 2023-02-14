0e8b7011c8d402a7f772958ab941502b80dc65df
Alek Novkovski <alek@greatcode2.com>
2023-02-14 17:15:01 +0100

    Fetch Router-Form data

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,4 +1,5 @@
 import AuthForm from '../components/AuthForm';
+import {json} from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
@@ -7,5 +8,8 @@ function AuthenticationPage() {
 export default AuthenticationPage;
 
 export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
   console.log(request)
 }
