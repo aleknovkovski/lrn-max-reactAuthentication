ae8ea82c06681532d0f34f960a3d3df91166080b
Alek Novkovski <alek@greatcode2.com>
2023-02-14 17:55:33 +0100

    Add error handling

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -17,6 +17,10 @@ export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get('mode') || 'login';
 
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
   const response = await fetch('http://localhost:8080/' + mode, {
     method: 'POST',
     headers: {
@@ -24,6 +28,14 @@ export async function action({ request }) {
     },
     body: JSON.stringify(authData),
   });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
 
   console.log(response)
   return redirect('/');
