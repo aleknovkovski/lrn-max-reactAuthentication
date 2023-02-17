77e5a09ea79f411d7ca3d94980e784f52b78b498
Alek Novkovski <alek@greatcode2.com>
2023-02-17 23:29:50 +0100

    Logout user if token expired

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,28 @@
-import {Outlet, useNavigation} from 'react-router-dom';
+import {Outlet, useLoaderData, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {getTokenDuration} from '../util/auth';
+import {useEffect} from "react";
 
 function RootLayout() {
     // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        if (token === 'EXPIRED') {
+            submit(null, { action: '/logout', method: 'post' });
+            return;
+        }
+
+        const tokenDuration = getTokenDuration();
+        console.log(tokenDuration);
+
+    }, [token, submit]);
 
     return (
         <>
